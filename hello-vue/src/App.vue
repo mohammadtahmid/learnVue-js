1fcdae32123f78deffa095777f18b5c19c2a51a8
mohammadtahmid <tahmidalmaruf99@gmail.com>
2025-01-20 18:50:06 +0600

    install vue project

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <my-component title="This is title"></my-component>
+    <p>{{ name }}</p>
+    <!-- <MyComponent/> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    
+     
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import test from "./components/Test";
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyComponent:test
+  },
+  data() {
+    return {
+      name: 'VueJs Tutorial'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-vue/src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="test">
+        <h3>My test Component</h3>
+        <h4>{{ title }}</h4>
+    </div>
+</template>
+
+
+
+<script>
+    export default {
+        name:'Test',
+        props:['title']
+    }
+</script>
+
+
+<style>
+
+</style>
