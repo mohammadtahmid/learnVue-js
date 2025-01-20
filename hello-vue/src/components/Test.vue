e5737f43dd5060738533b2eac9f7153a59a152bb
mohammadtahmid <tahmidalmaruf99@gmail.com>
2025-01-20 19:51:36 +0600

    lifeCycle Hooks

--- hello-vue/src/components/Test.vue
+++ hello-vue/src/components/Test.vue
@@ -2,6 +2,8 @@
     <div id="test">
         <h3>My test Component</h3>
         <h4>{{ title }}</h4>
+        <h1>{{ name }}</h1>
+        <button @click.prevent="myButton()">Click</button>
     </div>
 </template>
 
@@ -10,7 +12,31 @@
 <script>
     export default {
         name:'Test',
-        props:['title']
+        props:['title'],
+
+        // beforeCreate(){
+        //     alert('This is before create hook')
+        // },
+        // created(){
+        //     alert('This is create hook')
+        // },
+
+
+        data(){
+            return{
+                name: 'Maruf'
+            }
+        },
+
+        methods:{
+            myButton(){
+                this.name = 'Tahmid'
+            }
+        },
+
+        updated(){
+            alert('updated')
+        }
     }
 </script>
 
