ed5e3d3c15389f29856b5c7d301c9ff87455e3c3
mohammadtahmid <tahmidalmaruf99@gmail.com>
2025-01-22 01:47:26 +0600

    laravel vue vite

---
+++ laravel-vite/resources/js/App.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h3>Hello I am from VueJs App</h3>
+        <h3>Hello I am from VueJs App</h3>
+        <h3>Hello I am from VueJs App</h3>
+        <h3>Hello I am from VueJs App</h3>
+        <h3>Hello I am from VueJs App</h3>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
